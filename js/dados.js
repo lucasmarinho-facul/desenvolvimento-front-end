f42fdfd29bab16e43b216916305af23c29b8a211
lucasmarinho-facul <lucas.marinho@rede.ulbra.br>
2026-09-07 17:22:13 -0300

    Add files via upload

---
+++ js/dados.js
@@ -0,0 +1,9 @@
+export const tarefas = [
+    {
+        id: 1,
+        titulo: "Modelar banco de dados",
+        status: "a-fazer",
+        prioridade: "alta",
+        prazo: "15/06/2026"
+    }
+];
